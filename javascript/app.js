0905837acd16da52fbbeb8830d5e3c6ee640c95c
Paola Stone <paola.stonemartinez@dell.com>
2019-07-28 01:47:09 -0400

    counters and timers added

--- javascript/app.js
+++ javascript/app.js
@@ -4,7 +4,9 @@ var questionsArray = [
     {
         question: "Who was Ross's second wife?",
         choices: ["Monica" , "Emma",  "Emily",  "Rachel" ],
-        answer: ["Emily", "2"]
+        answer: ["Emily", "2"],
+        imageRight: "correct1.gif",
+        imageWrong: "images\wrong1.gif",
     },
 
     {
@@ -55,23 +57,46 @@ var questionsArray = [
 
 ]
 
+var rightAnswers = 0;
+var wrongAnswers = 0;
+var noAnswers = 0;
+
 $("#start").click(startQuestions);
+
 
 var number = 10;
 var intervalId;
 var i = 0;
 
-function startQuestions(){
-    if (i >= 10){
-        alert("End Of Game");
-    };
 
+
+function gameAreaHide() {
+    var x = document.getElementById("gameArea");
+    x.style.display = "none";
+}
+
+function gameAreaShow() {
+    var x = document.getElementById("gameArea");
+    x.style.display = "block";
+}
+
+
+function startQuestions(){
+    if (i > 9){
+        stop();
+        alert("End Of Game");
+
+    };
+    imagesHide();
+    timerShow();
+    gameAreaShow();
+       
     $("#question").text(questionsArray[i].question);
     $("#option1").text(questionsArray[i].choices[0]);
     $("#option2").text(questionsArray[i].choices[1]);
     $("#option3").text(questionsArray[i].choices[2]);
     $("#option4").text(questionsArray[i].choices[3]);
-    
+        
     nextQuestion();
     run();
 
@@ -80,41 +105,86 @@ function startQuestions(){
 
 function nextQuestion() {
     i++;
-    number = 10;
+    number = 10;  
 }
 
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-  }
+}
  
   
 function decrement() {
     number--;
     $("#timer").html("Time remaining: " + number);
     if (number === 0) {
+        noAnswers++
+        console.log(noAnswers)
         stop();
-        console.log("Time Up! The right answer was: " + questionsArray[i-1].answer[0])
+        $("#timer").html(" ");
+        $("#timer").html("Time's up!  The right answer was: " + questionsArray[i-1].answer[0]);
+        gameAreaHide();
+        imagesShow()
+        delay();
       }
     }
 
 function stop() {
     clearInterval(intervalId);
+    
+}
+
+function imagesHide(){
+    var x = document.getElementById("image");
+    x.style.display = "none";
+}
+
+function imagesShow(){
+    var x = document.getElementById("image");
+    x.style.display = "block";
+}
+
+function timerHide(){
+    var x = document.getElementById("timer");
+    x.style.display = "none";
+}
+
+function timerShow(){
+    var x = document.getElementById("timer");
+    x.style.display = "block";
 }
 
 
+function delay(){
+    setTimeout(startQuestions,3000)
+}
 
 function selection(){
 $(".button").click(function() {
-    if($(".button").is(":checked")) { 
-        console.log("it's checked"); 
-        console.log(this.value);
-        console.log(questionsArray[i-1].answer[1])
+    if($(".button").on("click")) { 
         if (this.value === questionsArray[i-1].answer[1]) {
-            console.log(questionsArray[i-1].answer[0] + "  is the right answer");
-        }else{
-            console.log("Wrong! The correct answer is " + questionsArray[i-1].answer[0]);
+            rightAnswers++
+            console.log(rightAnswers)
+            stop();
+            $("#timer").html(" ");
+            $("#timer").html(questionsArray[i-1].answer[0] + "  is the right answer");
+            gameAreaHide();
+            imagesShow();
+            delay();
+            
 
+
+            // $("#image").append("<img src= 'images/correct1.gif' />");////Show the image///
+        }else{
+            wrongAnswers++
+            console.log(wrongAnswers)
+            stop();
+            $("#timer").html(" ");
+            $("#timer").html("Wrong! The correct answer is " + questionsArray[i-1].answer[0] );
+            gameAreaHide();
+            imagesShow();
+            delay();
+           
         }
     };
 
